8dd8fe4c221776114125b8b2933e3d2f328d9ce1
erik0910 <erikmartinez0910@gmail.com>
2020-04-02 17:29:52 +0200

    añadidas nuevas funcionalidades ademas de constructores para cada tipo  de usario

--- my-app/src/main/java/com/mycompany/app/Users.java
+++ my-app/src/main/java/com/mycompany/app/Users.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+import java.util.ArrayList;
+
 public class Users {
 	//tipos de attributos que van a componer el proyecto
 private  String nombre;
@@ -7,8 +9,8 @@ private  String direccion;
 private  String email;
 private  String pass;
 private   Type type;
-
-private enum Type { Admin,trabajador}//definimos el tipo de Usuario que va a ser
+private ArrayList<Users>trabajadores;// este atributo solo se usara si el usurio es de tipo Admin ya que aqui almacenara todos sus trabajadores
+public  enum Type { Admin,trabajador}//definimos el tipo de Usuario que va a ser
 
 public String getNombre() {
 	return nombre;
@@ -56,16 +58,36 @@ public String toString() {
 			+ type + "]";
 }
 //por defecto va a ser la clase trabajador 
-public Users(String nombre, String direccion, String email, String pass, Type type) {
+public Users(String nombre, String direccion, String email, String pass) {
 	super();
 	this.nombre = nombre;
 	this.direccion = direccion;
 	this.email = email;
 	this.pass = pass;
-	this.type = type.trabajador;
+	this.type = Type.trabajador;
+	
 }
-
-
+//este constructor se declara para la creación de un administrados añadiendo el parametro de la lista de usuarios que va a tener a su nombre
+public Users(String nombre, String direccion, String email, String pass,ArrayList<Users>trabajadores) {
+	super();
+	this.nombre = nombre;
+	this.direccion = direccion;
+	this.email = email;
+	this.pass = pass;
+	this.type = Type.Admin;
+	this.trabajadores=trabajadores;
+	
+}
+//método que se usara para los usario de tipo Admin
+public void anyadirTrabajador(Users usuario) {
+	//verificamos que el usairo que vamos a añadir en la lista es un usariok de tipo trabajador
+	if(usuario.type==Type.trabajador) {
+		
+		trabajadores.add(usuario);
+	}
+	
+	
+}
 
 
 
