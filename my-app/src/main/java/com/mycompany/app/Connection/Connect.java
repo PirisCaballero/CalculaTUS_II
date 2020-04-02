67b605a0195b988cff3795d068337fdcb2724b37
PirisCaballero <aitorpiris@opendeusto.es>
2020-04-02 20:00:11 +0200

    The Connection object say hello if coenct true

--- my-app/src/main/java/com/mycompany/app/Connection/Connect.java
+++ my-app/src/main/java/com/mycompany/app/Connection/Connect.java
@@ -1,14 +1,44 @@
+package com.mycompany.app.Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Connect {
 	
-	try {
-	    Class.forName("com.mysql.jdbc.Driver");
-	} catch (ClassNotFoundException ex) {
-	    System.out.println("Error al registrar el driver de MySQL: " + ex);
+	Connection conn;
+	public Connect () {
+		try {
+		    Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException ex) {
+		    System.out.println("Error al registrar el driver de MySQL: " + ex);
+		}
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://83.213.204.144:3306/calculatus_root","calculaTUS_root", "Nevera98!");
+			boolean ok = say_hello(conn);
+			System.out.println(ok);
+		}catch(SQLException sqlE) {
+			System.out.println(sqlE);
+			sqlE.printStackTrace();
+		}
+	}
+	public boolean say_hello(Connection cn) {
+		try {
+			if(cn.isValid(50000)) {
+				return true;
+			}else {
+				return false;
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
-	public boolean say_hello () {
-		
+	
+	
+	public static void main(String[] args) {
+		Connect c = new Connect();
 	}
-
 }
