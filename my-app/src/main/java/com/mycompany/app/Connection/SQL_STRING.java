391ce8c2e9304935209bae859c637fa3a55d33d6
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-04-03 18:54:08 +0200

    format + organize imports

--- my-app/src/main/java/com/mycompany/app/Connection/SQL_STRING.java
+++ my-app/src/main/java/com/mycompany/app/Connection/SQL_STRING.java
@@ -1,10 +1,9 @@
 package com.mycompany.app.Connection;
 
+public class SQL_STRING {
 
-public class SQL_STRING{
-	
 	public SQL_STRING(String sentencia) {
-		
+
 	}
 
 }
--- my-app/src/main/java/com/mycompany/app/ventanas/VentanaLogin.java
+++ my-app/src/main/java/com/mycompany/app/ventanas/VentanaLogin.java
@@ -1,10 +1,15 @@
 package com.mycompany.app.ventanas;
-import java.awt.Color;
-import java.awt.Rectangle;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 import com.mycompany.app.Users;
 import com.mycompany.app.Connection.Connect;
@@ -13,7 +18,9 @@ public class VentanaLogin extends JFrame {
 
 	private JLabel lblUser, lblPass;
 	private JButton btnAceptar, btnRegistrar;
-	private JTextField txtUsuario; private JPasswordField txtPassword;
+	private JTextField txtUsuario;
+	private JPasswordField txtPassword;
+
 	public VentanaLogin() {
 		super("Login de usuario");
 		this.setSize(600, 400);
@@ -28,77 +35,75 @@ public class VentanaLogin extends JFrame {
 		lblPass = new JLabel();
 		txtUsuario = new JTextField();
 		txtPassword = new JPasswordField();
-		
+
 		btnAceptar = new JButton();
 		btnRegistrar = new JButton();
-		
+
 		btnAceptar.setText("Aceptar");
 		btnAceptar.setBounds(100, 40, 120, 30);
 		btnAceptar.setLocation(320, 250);
-		
+
 		btnRegistrar.setText("Registrarse");
 		btnRegistrar.setBounds(160, 40, 120, 30);
 		btnRegistrar.setLocation(140, 250);
-		
+
 		lblUser.setText("Introduce el nombre de usuario:");
 		lblPass.setText("Introduce la contrasena:");
-		
+
 		lblUser.setBounds(20, 10, 300, 20);
 		lblUser.setLocation(100, 100);
-		
+
 		txtUsuario.setBounds(20, 20, 200, 20);
 		txtUsuario.setLocation(300, 100);
-		
+
 		lblPass.setBounds(20, 10, 300, 20);
 		lblPass.setLocation(100, 140);
-		
+
 		txtPassword.setBounds(20, 80, 200, 20);
 		txtPassword.setLocation(300, 140);
-		
-		
+
 		add(txtUsuario);
 		add(txtPassword);
 		add(lblUser);
 		add(lblPass);
 		add(btnAceptar);
 		add(btnRegistrar);
-		
+
 		btnAceptar.addActionListener(new ActionListener() {
-			
+
 			@SuppressWarnings("deprecation")
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Comprobacion de usuario correcto");
 				Connect conn = new Connect();
-				Users user = conn.Verificar_usuario(txtUsuario.getText() , txtPassword.getText());
-				if( user != null ) {
-					JOptionPane.showMessageDialog(null, "Bienvenido de nuevo "+user.getNombre());
+				Users user = conn.Verificar_usuario(txtUsuario.getText(), txtPassword.getText());
+				if (user != null) {
+					JOptionPane.showMessageDialog(null, "Bienvenido de nuevo " + user.getNombre());
 					System.out.println(user.toString());
 					dispose();
-				}else {
+				} else {
 					JOptionPane.showMessageDialog(null, "Usuario no registrado");
 				}
-				
+
 			}
 		});
-		
+
 		btnRegistrar.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				VentanaRegistro vtnRegister = new VentanaRegistro();
 				dispose();
 			}
 		});
-	
+
 	}
-	
-	//main para probar la ejecucion de la ventana. Ventana principal, deriva al resto
+
+	// main para probar la ejecucion de la ventana. Ventana principal, deriva al
+	// resto
 	public static void main(String[] args) {
 		VentanaLogin a = new VentanaLogin();
 		a.setVisible(true);
 	}
-	
-	
-		
+
 }
