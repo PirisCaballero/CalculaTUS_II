c2ab52f8b9da3be87d9c86ebaa352247d7680e97
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-04-03 14:11:11 +0200

    ventana consola practicamente hecho, falta consensuar con los demas

--- my-app/src/main/java/com/mycompany/app/ventanas/VentanaConsola.java
+++ my-app/src/main/java/com/mycompany/app/ventanas/VentanaConsola.java
@@ -1,19 +1,26 @@
 package com.mycompany.app.ventanas;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
 public class VentanaConsola {
 
 	JButton botonAtras, botonSiguiente;
-	JTextField consola;
+	JTextArea consola;
+	JCheckBox checkConsola;
+
+	// JScrollPane sp;
 
 	public VentanaConsola() {
 
@@ -23,38 +30,76 @@ public class VentanaConsola {
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		// frame.setTitle("TITULO");
 
-		JPanel ventanaRegistro = new JPanel();
-		ventanaRegistro.setLayout(null);
-		frame.getContentPane().add(ventanaRegistro);
-		ventanaRegistro.setVisible(true);
+		JPanel ventanaConsola = new JPanel();
+		ventanaConsola.setLayout(null);
+		frame.getContentPane().add(ventanaConsola);
+		ventanaConsola.setVisible(true);
 
-		consola = new JTextField();
-		consola.setText("texto de prueba para la consola");
-		consola.setBounds(10, 10, 690, 200);
+		consola = new JTextArea();
+		// consola.setText("Inserte texto aqui...");
+		consola.setBounds(10, 10, 690, 300);
+		consola.setForeground(Color.BLACK);
+		consola.setBackground(Color.WHITE);
+		consola.setCaretColor(Color.BLACK);
+		Font f = new Font("Serif", Font.BOLD, 15);
+		consola.setFont(f);
+		consola.setLineWrap(true);
+		consola.setWrapStyleWord(true);
+		consola.setMargin(new Insets(10, 10, 10, 10));
+		consola.setCaretPosition(0);
+
+		checkConsola = new JCheckBox();
+		checkConsola.setText("Modo oscuro");
+		checkConsola.setBounds(10, 390, 100, 30);
+		checkConsola.setSelected(false);
 
 		botonAtras = new JButton();
 		botonAtras.setText("Atras");
 		botonAtras.setBounds(10, 340, 200, 30);
 
 		botonSiguiente = new JButton();
-		botonSiguiente.setText("Siguiente");
+		botonSiguiente.setText("Siguiente/PonemosBuscar?");
 		botonSiguiente.setBounds(500, 340, 200, 30);
 
-		ventanaRegistro.add(consola);
-		ventanaRegistro.add(botonAtras);
-		ventanaRegistro.add(botonSiguiente);
+		ventanaConsola.add(consola);
+		ventanaConsola.add(botonAtras);
+		ventanaConsola.add(botonSiguiente);
+		ventanaConsola.add(checkConsola);
+
+		checkConsola.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (checkConsola.isSelected() == true) {
+					consola.setForeground(Color.WHITE);
+					consola.setBackground(Color.BLACK);
+					consola.setCaretColor(Color.WHITE);
+				} else if (checkConsola.isSelected() == false) {
+					consola.setForeground(Color.BLACK);
+					consola.setBackground(Color.WHITE);
+					consola.setCaretColor(Color.BLACK);
+				}
+
+			}
+		});
 
 		botonAtras.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("BOTON ATRAS actionListener");
+				consola.setText("");
+				checkConsola.setSelected(false);
+				consola.setForeground(Color.BLACK);
+				consola.setBackground(Color.WHITE);
+				consola.setCaretColor(Color.BLACK);
+				consola.setCaretPosition(0);
 			}
 		});
 
 		botonSiguiente.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				System.out.println("BOTON SIGUIENTE");
 			}
 		});
 	}
