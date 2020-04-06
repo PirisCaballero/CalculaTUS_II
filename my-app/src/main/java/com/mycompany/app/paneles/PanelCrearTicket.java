d1a1bb64d0fdbaa78c93cd217ef307403123c1b3
IratxeCampo <iratxe.campo@opendeusto.es>
2020-04-06 13:30:46 +0200

    Añadidos campos para conectar a BD

--- my-app/src/main/java/com/mycompany/app/paneles/PanelCrearTicket.java
+++ my-app/src/main/java/com/mycompany/app/paneles/PanelCrearTicket.java
@@ -2,16 +2,20 @@ package com.mycompany.app.paneles;
 
 import java.awt.Choice;
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.*;
 
 import com.mycompany.app.Connection.Connect;
 
 public class PanelCrearTicket extends JPanel{
-	JLabel lblFechaEmisionTicket, lblLocalTicket, lblProductosTicket;
-	JTextField txtFechaEmisionTicket, txtTienda;
+	JLabel lblFechaEmisionTicket, lblLocalTicket, lblProductosTicket, lblUnidades;
+	JTextField txtFechaEmisionTicket, txtTienda, txtUnidades;
 	JButton btnGuardarTicket, btnAgregarProducto;
+	JTable tablaProductos;
 	Choice choiceProducto;
+	Object columnas []= {"Producto", "Precio", "Unidades"};
+	Object celdas[][]= new Object[3][10];
 	
 	public PanelCrearTicket() {
 		
@@ -23,37 +27,37 @@ public class PanelCrearTicket extends JPanel{
 		
 		lblFechaEmisionTicket = new JLabel();
 		lblFechaEmisionTicket.setText("Fecha de emision:");
-		lblFechaEmisionTicket.setBounds(90, 70, 140, 30);
+		lblFechaEmisionTicket.setBounds(90, 30, 140, 30);
 		this.add(lblFechaEmisionTicket);
 
 		txtFechaEmisionTicket = new JTextField();
 		txtFechaEmisionTicket.setText("");
-		txtFechaEmisionTicket.setBounds(260, 70, 170, 30);
+		txtFechaEmisionTicket.setBounds(260, 30, 170, 30);
 		this.add(txtFechaEmisionTicket);
 
 		lblLocalTicket = new JLabel();
 		lblLocalTicket.setText("Introduce la tienda de compra:");
-		lblLocalTicket.setBounds(51, 120, 180, 30);
+		lblLocalTicket.setBounds(51, 80, 180, 30);
 		this.add(lblLocalTicket);
 
 		txtTienda = new JTextField();
 		txtTienda.setText("");
-		txtTienda.setBounds(260, 120, 170, 30);
+		txtTienda.setBounds(260, 80, 170, 30);
 		this.add(txtTienda);
 
 		lblProductosTicket = new JLabel();
 		lblProductosTicket.setText("Introduce los productos:");
-		lblProductosTicket.setBounds(70, 175, 180, 30);
+		lblProductosTicket.setBounds(70, 135, 180, 30);
 		this.add(lblProductosTicket);
 
 		choiceProducto = new Choice();
 		Connect cn = new Connect();
-		choiceProducto.setBounds(260, 180, 170, 50);
+		choiceProducto.setBounds(260, 140, 170, 50);
 		this.add(choiceProducto);
 		
 		btnAgregarProducto = new JButton();
 		btnAgregarProducto.setText("Agregar");
-		btnAgregarProducto.setBounds(440, 180, 50, 10);
+		btnAgregarProducto.setBounds(440, 135, 50, 10);
 		btnAgregarProducto.setSize(80, 30);
 		this.add(btnAgregarProducto);
 		
@@ -62,6 +66,22 @@ public class PanelCrearTicket extends JPanel{
 		btnGuardarTicket.setBounds(this.getWidth() / 2 - 50, 320, 50, 10);
 		btnGuardarTicket.setSize(80, 30);
 		this.add(btnGuardarTicket);
+		
+		lblUnidades = new JLabel();
+		lblUnidades.setText("Introduce las unidades:");
+		lblUnidades.setBounds(70, 185, 180, 30);
+		this.add(lblUnidades);
+		
+		txtUnidades = new JTextField();
+		txtUnidades.setText("");
+		txtUnidades.setBounds(260, 185, 170, 30);
+		this.add(txtUnidades);
+		
+		tablaProductos = new JTable(celdas, columnas);
+		tablaProductos.setBounds(20, 240, 200, 180);
+		tablaProductos.setSize(490, 100);
+		tablaProductos.setPreferredScrollableViewportSize(new Dimension(490, 100));
+		this.add(tablaProductos);
 
 	}
 }
