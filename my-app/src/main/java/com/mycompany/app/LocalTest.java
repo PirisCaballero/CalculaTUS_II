4e195e1102022ce9154487854c73f0cce1f0bd96
erik0910 <erikmartinez0910@gmail.com>
2020-04-06 11:20:24 +0200

    testing de la clase local finalizado y correcto

---
+++ my-app/src/main/java/com/mycompany/app/LocalTest.java
@@ -0,0 +1,40 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class LocalTest {
+	//comprobamos la funcionalidad de la clase Local
+	@Test
+	public void Funcionaltest() {
+		Local local=new Local("casa", "ninguna", 48190, "es un local");
+		//comprobación de los diferentes getters y setters funcionan correctamente
+		//getters
+		Boolean resultado;
+		resultado=local.getNombre().equals("casa");
+		assertTrue(resultado);
+		resultado=local.getDireccion().equals("ninguna");
+		assertTrue(resultado);
+		resultado=local.getDescripcion().equals("es un local");
+		assertTrue(resultado);
+		resultado=(local.getCp()==48190);
+		assertTrue(resultado);
+		//setters
+		local.setCp(0);
+		local.setDescripcion("nada");
+		local.setDireccion("santana");
+		local.setNombre("sopuerta");
+		resultado=local.getNombre().equals("sopuerta");
+		assertTrue(resultado);
+		resultado=local.getDireccion().equals("santana");
+		assertTrue(resultado);
+		resultado=local.getDescripcion().equals("nada");
+		assertTrue(resultado);
+		resultado=(local.getCp()==0);
+		assertTrue(resultado);
+		
+	}
+	
+
+}
