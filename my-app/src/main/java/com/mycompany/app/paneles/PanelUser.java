291edf59ff66079d8d97c0574595b34863e152b7
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-04-21 14:34:20 +0200

    Show_Descuentos vacio

--- my-app/src/main/java/com/mycompany/app/paneles/PanelUser.java
+++ my-app/src/main/java/com/mycompany/app/paneles/PanelUser.java
@@ -1,19 +1,17 @@
 package com.mycompany.app.paneles;
 
-import java.awt.Choice;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.util.ArrayList;
 
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 import com.mycompany.app.Local;
 import com.mycompany.app.Users;
-import com.mycompany.app.Connection.Connect;
+import com.mycompany.app.ventanas.Show_Descuentos;
 
 public class PanelUser extends JPanel {
 
@@ -24,6 +22,10 @@ public class PanelUser extends JPanel {
 	private ArrayList<Local> userList = new ArrayList<Local>();
 	private Panel_Datos pd;
 
+	private static Show_Descuentos sd;
+
+	private Users main_user;
+
 	public PanelUser(Users u, Panel_Datos pdts) {
 		this.setLayout(null);
 		this.setBorder(BorderFactory.createEtchedBorder());
@@ -33,17 +35,19 @@ public class PanelUser extends JPanel {
 		this.user = u;
 		this.pd = pdts;
 
-		btnDescuento = new JButton("Userrrrrrrrrrrrrrrrrrrrrrrrr");
-		btnDescuento.setBounds(178, 35, 200, 23);
+		btnDescuento = new JButton("Ver Descuentos");
+		btnDescuento.setBounds(178, 35, 200, 23); //se puede mover donde querais, estaba aqui pork el panel esta vacio
 		add(btnDescuento);
 
+		sd = new Show_Descuentos(main_user);
+		sd.setVisible(false);
 		btnDescuento.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				System.out.println("Refreshing...");
-
+				System.out.println("btnDescuento");
+				sd.setVisible(true);
 			}
 		});
 	}
