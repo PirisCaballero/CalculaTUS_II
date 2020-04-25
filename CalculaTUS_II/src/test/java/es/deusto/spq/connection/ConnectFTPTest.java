c2124eb20044a5c48cc4269ee581b8d056b1c0ef
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-04-25 19:02:19 +0200

    ConnectFTPTest VACIO!! + ConnectTest cambiado de package

---
+++ CalculaTUS_II/src/test/java/es/deusto/spq/connection/ConnectFTPTest.java
@@ -0,0 +1,68 @@
+package es.deusto.spq.connection;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.net.ftp.FTPFile;
+import org.junit.Test;
+
+public class ConnectFTPTest {
+
+	@Test
+	public String stripAccentsTest() {
+		fail("Not yet implemented");
+		return null;
+	}
+
+	@Test
+	public void ConnectFTPTest1() {
+		fail("Not yet implemented");
+	}	
+	
+	@Test
+	public boolean OpenConexionTest() {
+		fail("Not yet implemented");
+		return false;
+	}
+	
+	@Test
+	public boolean CloseConnectionTest() {
+		fail("Not yet implemented");
+		return false;
+	}
+	
+	@Test
+	public boolean RegisUserFolderTest() {
+		fail("Not yet implemented");
+		return false;
+	}
+	
+	@Test
+	public boolean UploadFileTest() {
+		fail("Not yet implemented");
+		return false;
+	}
+	
+	@Test
+	public boolean DownloadFileTest() {
+		fail("Not yet implemented");
+		return false;
+	}
+	
+	@Test
+	public FTPFile[] getUserFoldersTest() {
+		fail("Not yet implemented");
+		return null;
+	}
+	
+	@Test
+	public FTPFile[] getUserFilesTest() {
+		fail("Not yet implemented");
+		return null;
+	}
+	
+	@Test
+	public boolean createDirTest() {
+		fail("Not yet implemented");
+		return false;
+	}
+}
