2aacbe87edd5e4ac70c18cc6f110f25bad0a3362
IratxeCampo <iratxe.campo@opendeusto.es>
2020-04-26 15:15:35 +0200

    test de usuario terminado, problemas arreglados 100%

--- CalculaTUS_II/src/test/java/es/deusto/spq/UsersTest.java
+++ CalculaTUS_II/src/test/java/es/deusto/spq/UsersTest.java
@@ -5,16 +5,24 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import es.deusto.spq.Users;
+import es.deusto.spq.Users.Type;
 
 public class UsersTest {
 
 	@Test
 	public void test() {// comprobacion de los getters y seter de la clase usuario
 		Users usuario = new Users("erik", "martinez", "erik@gmail.com", "erik", 1, "null");
+		usuario.setAdmin(1);
+		Users u2 = new Users();
+		u2.setType(Type.trabajador);
+		assertTrue(u2.getType().equals(Type.trabajador));
+		u2.setDireccion("GRAN VIA");
+		assertTrue(u2.getDireccion().equals("GRAN VIA"));
+		usuario.setType(Type.Admin);
+		assertTrue(usuario.getType().equals(Type.Admin));
+		
 		Boolean resultado;
-		int a = usuario.getAdmin();
-		resultado = (a == 1);
-		assertTrue(resultado);
+		assertEquals(1, usuario.getAdmin());
 		String b = usuario.getAdminEmail();
 		resultado = b.equals("null");
 		assertTrue(resultado);
@@ -34,9 +42,7 @@ public class UsersTest {
 		usuario.setEmail("nada");
 		usuario.setPass("pass");
 		usuario.setNombre("nadie");
-		int a2 = usuario.getAdmin();
-		resultado = (a2 == 0);
-		assertTrue(resultado);
+		assertEquals(0, usuario.getAdmin());
 		String b2 = usuario.getAdminEmail();
 		resultado = b2.equals("erik@gmail.com");
 		assertTrue(resultado);
@@ -52,6 +58,8 @@ public class UsersTest {
 		String f = usuario.getEmail();
 		resultado = f.equals("nada");
 		assertTrue(resultado);
+
+		assertTrue(usuario.toString().equals("Users [nombre=nadie, apellidos=martinez, email=nada, pass=pass, admin=0, adminemail=erik@gmail.com"));
 
 	}
 
