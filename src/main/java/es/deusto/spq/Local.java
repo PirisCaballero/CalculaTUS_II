012da4582f518948300907621162d4d0a6af3ea1
IratxeCampo <iratxe.campo@opendeusto.es>
2020-05-07 21:52:44 +0200

    Prueba commit

--- src/main/java/es/deusto/spq/Local.java
+++ src/main/java/es/deusto/spq/Local.java
@@ -7,7 +7,7 @@ public class Local {
 	private int cp;
 	private String descripcion;
 	private int IdLocal;
-	private String emailDuenio;
+	private String emailDuenio; //super comentario mega elaborado
 
 	public Local(String nom, String dir, int codPost, String desc) {
 		this.nombre = nom;
