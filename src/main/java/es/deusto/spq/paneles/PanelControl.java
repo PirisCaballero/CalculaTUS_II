d7bb5562297251208fe9118e21742bf324daa972
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-05-08 13:26:23 +0200

    a

--- src/main/java/es/deusto/spq/paneles/PanelControl.java
+++ src/main/java/es/deusto/spq/paneles/PanelControl.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-import javax.activation.MailcapCommandMap;
+//import javax.activation.MailcapCommandMap;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
