cae7fce15e6277ba5f9acc80657f8a8dd68d4256
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2020-05-17 21:39:31 +0200

    un poco de mockito

--- src/test/java/es/deusto/spq/paneles/PanelFTPTest.java
+++ src/test/java/es/deusto/spq/paneles/PanelFTPTest.java
@@ -1,6 +1,8 @@
 package es.deusto.spq.paneles;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -9,11 +11,15 @@ import org.junit.Test;
 import es.deusto.spq.Users;
 import es.deusto.spq.connection.Connect;
 
+import static org.mockito.Mockito.*;
+
 public class PanelFTPTest {
 	
 	static Connect c;
 	static Users u;
 	static PanelFTP pftp;
+	
+	PanelFTP pftp2 = mock(PanelFTP.class);
 	
 	@Before
 	public void setUp() {
@@ -26,5 +32,31 @@ public class PanelFTPTest {
 		pftp = new PanelFTP(u);
 		assertFalse(pftp.equals(null));
 	}
+	
+	@Test
+	public void testDownload() {
+		when(pftp2.Download()).thenReturn(true);
+		assertTrue(pftp2.Download());
+	}
+	
+	@Test
+	public void testUpload() {
+		when(pftp2.Upload()).thenReturn(true);
+		assertTrue(pftp2.Upload());
+	}
+	
+	@Test
+	public void testAddFolder() {
+		when(pftp2.AddFolder()).thenReturn(true);
+		assertTrue(pftp2.AddFolder());
+	}
+	
+	@Test
+	public void testAddFolderFunc() {
+		when(pftp2.addFolderFunc(null)).thenReturn(true);
+		assertTrue(pftp2.addFolderFunc(null));
+	}
+	
+	
 	
 }
