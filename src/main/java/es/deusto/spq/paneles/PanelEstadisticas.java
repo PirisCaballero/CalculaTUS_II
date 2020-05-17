29b722a51048869e5e1758047a7daf2c4bd3be1c
IratxeCampo <iratxe.campo@opendeusto.es>
2020-05-18 01:51:28 +0200

    PanelEstadisticas terminado, pendiente arreglos

--- src/main/java/es/deusto/spq/paneles/PanelEstadisticas.java
+++ src/main/java/es/deusto/spq/paneles/PanelEstadisticas.java
@@ -14,6 +14,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
@@ -44,6 +45,23 @@ public class PanelEstadisticas extends JPanel {
 	private ArrayList<Ticket> tckArr ;
 	Local l;
 	
+	double contadorAlimentacion = 0.0;
+	double contadorSalud = 0.0;
+	double contadorOcio = 0.0;
+	double contadorTecnologia = 0.0;
+	double contadorFacturas = 0.0;
+	double contadorRopa = 0.0;
+	double totalAlimentacion = 0.0;
+	double totalSalud = 0.0;
+	double totalOcio = 0.0;
+	double totalTecnologia = 0.0;
+	double totalFacturas = 0.0;
+	double totalRopa = 0.0;
+	
+	JProgressBar pgbAlimentacion, pgbSalud, pgbOcio, pgbTecnologia, pgbFacturas, pgbRopa;
+	
+	JLabel lblAlimentacion, lblSalud, lblOcio, lblTecnologia, lblFacturas, lblRopa;
+	
 	Connect cn;
 	
 	Choice chTienda;
@@ -72,18 +90,18 @@ public class PanelEstadisticas extends JPanel {
 		add(lblTitulo);
 		
 		lblTienda = new JLabel("Introduce la tienda por la que quieres filtrar:");
-		lblTienda.setBounds(10, 130, 250, 30);
+		lblTienda.setBounds(10, 100, 250, 30);
 		add(lblTienda);
 		
 		lblTipoProducto = new JLabel("Introduce el tipo de producto a filtrar:");
-		lblTipoProducto.setBounds(10, 170, 250, 30);
+		lblTipoProducto.setBounds(10, 150, 250, 30);
 		add(lblTipoProducto);
 		
 		
 		cn = new Connect();
 		
 		chTienda = new Choice();
-		chTienda.setBounds(270, 130, 200, 20);
+		chTienda.setBounds(270, 100, 200, 20);
 		arrayLocal = cn.getLocales(u);
 		for(Local t: arrayLocal) {
 			chTienda.add(t.getNombre());
@@ -91,19 +109,73 @@ public class PanelEstadisticas extends JPanel {
 		add(chTienda);
 		
 		chTipoProducto = new Choice();
-		chTipoProducto.setBounds(270, 170, 200, 20);
+		chTipoProducto.setBounds(270, 150, 200, 20);
 		for(String s: cn.getCategorias()) {
 			chTipoProducto.add(s);
 		}
 		add(chTipoProducto);
 		
 		btnBuscar = new JButton("Buscar");
-		btnBuscar.setBounds(140, 280, 80, 30);
+		btnBuscar.setBounds(140, 190, 80, 30);
 		add(btnBuscar);
 		
 		btnRefrescar = new JButton("Refresh");
-		btnRefrescar.setBounds(260, 280, 80, 30);
+		btnRefrescar.setBounds(260, 190, 80, 30);
 		add(btnRefrescar);
+		
+		lblAlimentacion = new JLabel("Alimentación");
+		lblAlimentacion.setBounds(60, 240, 150, 20);
+		add(lblAlimentacion);
+		
+		pgbAlimentacion = new JProgressBar();
+		pgbAlimentacion.setBounds(240, 240, 200, 20);
+		pgbAlimentacion.setStringPainted(true);
+		add(pgbAlimentacion);
+		
+		lblFacturas = new JLabel("Facturas");
+		lblFacturas.setBounds(60, 270, 150, 20);
+		add(lblFacturas);
+		
+		pgbFacturas = new JProgressBar();
+		pgbFacturas.setBounds(240, 270, 200, 20);
+		pgbFacturas.setStringPainted(true);
+		add(pgbFacturas);
+		
+		lblOcio = new JLabel("Ocio");
+		lblOcio.setBounds(60, 300, 150, 20);
+		add(lblOcio);
+		
+		pgbOcio = new JProgressBar();
+		pgbOcio.setBounds(240, 300, 200, 20);
+		pgbOcio.setStringPainted(true);
+		add(pgbOcio);
+		
+		lblRopa = new JLabel("Ropa");
+		lblRopa.setBounds(60, 330, 150, 20);
+		add(lblRopa);
+		
+		pgbRopa = new JProgressBar();
+		pgbRopa.setBounds(240, 330, 200, 20);
+		pgbRopa.setStringPainted(true);
+		add(pgbRopa);
+		
+		lblSalud = new JLabel("Salud");
+		lblSalud.setBounds(60, 360, 150, 20);
+		add(lblSalud);
+		
+		pgbSalud = new JProgressBar();
+		pgbSalud.setBounds(240, 360, 200, 20);
+		pgbSalud.setStringPainted(true);
+		add(pgbSalud);
+		
+		lblTecnologia = new JLabel("Tecnología");
+		lblTecnologia.setBounds(60, 390, 150, 20);
+		add(lblTecnologia);
+		
+		pgbTecnologia = new JProgressBar();
+		pgbTecnologia.setBounds(240, 390, 200, 20);
+		pgbTecnologia.setStringPainted(true);
+		add(pgbTecnologia);
 		
 		btnRefrescar.addActionListener(new ActionListener() {
 			
@@ -194,18 +266,34 @@ public class PanelEstadisticas extends JPanel {
 		}
 		
 		prMios = cn.getProductsByUser(u);
-		int contador = 0;
-		double total = 0;
+		
 		for (int i = 0; i < prMios.size(); i++) {
 			System.out.println(prMios.get(i).getCategoria());
-			if(prMios.get(i).getCategoria()==chTipoProducto.getSelectedItem()) {
-				contador += 1;
-			}
+			if(prMios.get(i).getCategoria().equals("Alimentacion"))
+				contadorAlimentacion = contadorAlimentacion + 1;
+			else if(prMios.get(i).getCategoria()=="Salud/Higiene") contadorSalud = contadorSalud + 1;
+			else if(prMios.get(i).getCategoria()=="Ocio") contadorOcio = contadorOcio + 1 ;
+			else if(prMios.get(i).getCategoria()=="Tecnologia") contadorTecnologia = contadorTecnologia + 1 ;
+			else if(prMios.get(i).getCategoria()=="Facturas") contadorFacturas = contadorFacturas + 1 ;
+			else if(prMios.get(i).getCategoria()=="Ropa") contadorRopa = contadorRopa + 1 ;
 			
 		}
-		total = contador/prMios.size()*100;
-		System.out.println(total);
-			
+		
+		totalAlimentacion = contadorAlimentacion/prMios.size()*100;
+		totalFacturas = contadorFacturas/prMios.size()*100;
+		totalOcio = contadorOcio/prMios.size()*100;
+		totalRopa = contadorRopa/prMios.size()*100;
+		totalSalud = contadorSalud/prMios.size()*100;
+		totalTecnologia = contadorTecnologia/prMios.size()*100;
+		
+		pgbAlimentacion.setValue((int)totalAlimentacion);
+		pgbFacturas.setValue((int)totalFacturas);
+		pgbOcio.setValue((int)totalOcio);
+		pgbRopa.setValue((int)totalRopa);
+		pgbSalud.setValue((int)totalSalud);
+		pgbTecnologia.setValue((int)totalTecnologia);
+		
+		
 //		System.out.println(chTienda.getSelectedItem());
 //		System.out.println(chTipoProducto.getSelectedItem());
 	}
